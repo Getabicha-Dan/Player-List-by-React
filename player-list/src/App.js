8987f88fff83022425bd8c5421d05a1bcbb94af3
Getabicha Daniel <getabicha.weldekidan@gmail.com>
2023-01-19 13:59:43 +0200

    New project react playerSS

---
+++ player-list/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  const titel = 'The player list'
+  return (
+    <div className="home">
+      <h1>{titel}</h1>
+    </div>
+  );
+}
+
+export default App;
